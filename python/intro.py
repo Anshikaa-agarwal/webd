425e6b3630b2ec00a0aeae75b3632ab50fa00282
Anshikaa-agarwal <anshika2020ag@gmail.com>
2025-06-25 21:35:45 +0530

    intro to python

---
+++ python/intro.py
@@ -0,0 +1,25 @@
+print("hello")
+# try:
+#     a = int(input("Enter a number: "))
+#     print(a)
+# except ValueError:
+#     print("Please enter a valid number.")
+
+print(4/3)
+print(4//3)
+
+print(4 == "4")
+   
+sum=0
+i=0
+# while i<10:
+#     if i%2 !=0:
+#         sum += i
+#     i += 1
+    
+for i in range (1,11,1):
+    if (i%2 != 0):
+        sum += i
+        
+print(sum)
+
